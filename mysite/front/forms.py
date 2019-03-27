39f27a7ad1fb8e72bef487e4f7223f761c518e3a
Shirali Shah <shirali@edcast.com>
2019-03-27 15:39:38 +0000

    removed duplicated from db and made form and filter work with db

--- mysite/front/forms.py
+++ mysite/front/forms.py
@@ -2,21 +2,24 @@ from django import forms
 
 class choiceform(forms.Form):
     loc = forms.ChoiceField(label = "Location", choices = ( \
-                                        ('UK','Scotland'),\
-                                        ('US','United States'),\
-                                        ('WA','Wales')))
+                                        ('NA', 'Select'),\
+                                        ('UK','United Kingdom'),\
+                                        ('US','United States')))
 
 
-    deg = forms.ChoiceField(label = "Degree level", choices = ( \
-                                        ('XyZ','BSc'),\
-                                        ('YzX','MSc'),\
-                                        ('ZxY','Phd')))
+    stud = forms.ChoiceField(label = "Number of Students", choices = ( \
+                                        ('any', 'Select'),\
+                                        ('small','< 5,000'),\
+                                        ('med','5,000 - 15,000'),\
+                                        ('large','> 15,000')))
 
 
-    sub = forms.ChoiceField(label = "Subject", choices = ( \
-                                        ('AbC','Informatics'),\
-                                        ('BcA','Physics'),\
-                                        ('CaB','Chemistry')))
+    tuit = forms.ChoiceField(label = "Tuition", choices = ( \
+                                        ('all', 'Select'),\
+                                        ('less','< $15,000'),\
+                                        ('midless','$15,000 - $30,000'),\
+                                        ('midmore','$30,000-$45,000'),\
+                                        ('more', '> $45,000')))
 
     def process(self):
         location = self.cleaned_data
